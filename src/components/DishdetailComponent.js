6ebb81b47d15288d605a334680e084a9fd3f340e
Teghdeep Kapoor <teghdeep@gmail.com>
2020-05-07 10:14:06 +0530

    Header and Footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,58 +7,47 @@ import {
   CardTitle,
   Media,
 } from "reactstrap";
-import { DISHES } from "C:/Users/Deepa Kapoor/REACT PROJECTS/React/confusion/src/shared/dishes";
-class Dishdetail extends Component {
-  constructor(props) {
-    super(props);
-    this.setState({ dishes: DISHES });
-  }
 
-  render() {
-    if (this.props.dish != null) {
-      const comment = this.props.dish.comments.map((comment) => {
-        return (
-          <Media>
-            <Media body>
-              {comment.comment}
-              <br />
-              <br />
-              --{comment.author},
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              }).format(new Date(Date.parse(comment.date)))}
-              <br />
-              <br />
-            </Media>
-          </Media>
-        );
-      });
-
+const Dishdetail = (props) => {
+  if (props.dish != null) {
+    const comment = props.dish.comments.map((comment) => {
       return (
-        <>
-          <div className="col-12 col-md-5 m-1">
-            <Card>
-              <CardImg
-                top
-                src={this.props.dish.image}
-                alt={this.props.dish.name}
-              />
-              <CardBody>
-                <CardTitle>{this.props.dish.name}</CardTitle>
-                <CardText>{this.props.dish.description}</CardText>
-              </CardBody>
-            </Card>
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            <h2>Comments</h2>
-            {comment}
-          </div>
-        </>
+        <Media>
+          <Media body>
+            {comment.comment}
+            <br />
+            <br />
+            --{comment.author},
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            }).format(new Date(Date.parse(comment.date)))}
+            <br />
+            <br />
+          </Media>
+        </Media>
       );
-    } else return <div></div>;
-  }
-}
+    });
+
+    return (
+      <>
+        <div className="col-12 col-md-5 m-1">
+          <Card>
+            <CardImg top src={props.dish.image} alt={props.dish.name} />
+            <CardBody>
+              <CardTitle>{props.dish.name}</CardTitle>
+              <CardText>{props.dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          <h2>Comments</h2>
+          {comment}
+        </div>
+      </>
+    );
+  } else return <div></div>;
+};
 
 export default Dishdetail;
