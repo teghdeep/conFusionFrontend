322e980e938a0c4c158fe8486b9f5f0bcbdb5cb9
Teghdeep Kapoor <teghdeep@gmail.com>
2020-05-07 08:23:08 +0530

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -23,7 +23,12 @@ class Dishdetail extends Component {
               {comment.comment}
               <br />
               <br />
-              --{comment.author},{comment.date}
+              --{comment.author},
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              }).format(new Date(Date.parse(comment.date)))}
               <br />
               <br />
             </Media>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,30 +1,20 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardTitle,
-} from "reactstrap";
-import Dishdetail from "./DishdetailComponent"
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 class Menu extends Component {
   constructor(props) {
     super(props);
 
-    this.state = ({
+    this.state = {
       selectedDish: null,
-    });
-  };
-
-  onDishSelect(dish) {
-    this.setState({ selectedDish: dish });
-  };
+    };
+  }
 
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div className="col-12 col-md-5 m-1">
-          <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+          <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
@@ -37,12 +27,9 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-        <div className="row">
-          <Dishdetail dish={this.state.selectedDish} />
-        </div>
       </div>
     );
-    }
+  }
 }
 
 export default Menu;
