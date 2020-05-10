739b39e49d182feea0a47a711962fd681b024424
Teghdeep Kapoor <teghdeep@gmail.com>
2020-05-10 12:48:58 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -20,6 +20,7 @@ import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 class Dishdetail extends Component {
   constructor(props) {
@@ -55,21 +56,24 @@ class Dishdetail extends Component {
     const comment = this.props.comments.map((comment) => {
       return (
         <>
-          <Media>
-            <Media body>
-              {comment.comment}
-              <br />
-              <br />
-              --{comment.author},
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              }).format(new Date(Date.parse(comment.date)))}
-              <br />
-            </Media>
-          </Media>
-
+          <Stagger in>
+            <Fade in>
+              <Media>
+                <Media body>
+                  {comment.comment}
+                  <br />
+                  <br />
+                  --{comment.author},
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit",
+                  }).format(new Date(Date.parse(comment.date)))}
+                  <br />
+                </Media>
+              </Media>
+            </Fade>
+          </Stagger>
           <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
             <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
             <ModalBody>
@@ -190,18 +194,26 @@ class Dishdetail extends Component {
           </div>
           <div className="row">
             <div className="col-12 col-md-5 m-1">
-              <Card>
-                <CardImg
-                  top
-                  src={baseUrl + this.props.dish.image}
-                  alt={this.props.dish.name}
-                />
-                <CardBody>
-                  <CardTitle>{this.props.dish.name}</CardTitle>
-                  <CardText>{this.props.dish.description}</CardText>
-                </CardBody>
-              </Card>
+              <FadeTransform
+                in
+                transformProps={{
+                  exitTransform: "scale(0.5) translateY(-50%)",
+                }}
+              >
+                <Card>
+                  <CardImg
+                    top
+                    src={baseUrl + this.props.dish.image}
+                    alt={this.props.dish.name}
+                  />
+                  <CardBody>
+                    <CardTitle>{this.props.dish.name}</CardTitle>
+                    <CardText>{this.props.dish.description}</CardText>
+                  </CardBody>
+                </Card>
+              </FadeTransform>
             </div>
+
             <div className="col-12 col-md-5 m-1">
               <h2>Comments</h2>
               {comment}
